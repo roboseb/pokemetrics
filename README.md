6629254bedf75a1f9237fddcdd2ac77f9769dc0d
roboseb <sebastienapril@rocketmail.com>
2023-10-25 15:58:22 -0600

    Add second fetch request for a random pokemon. Add conversion function to convert from cm to pokemon tall.

--- README.md
+++ README.md
@@ -22,15 +22,25 @@ October 24th, 2023
 
     Hopefully when I make progress I remember to record it so I can maybe make a video.
 
+October 25th, 2023
+
+    Hoping to crank out some more progress today as I'd like to have this done around the end of next week. With the basic inputs I have set up I can hopefully have the app tell you how many of x pokemon you are tall/heavy fairly quickly.
+
+    We now have a system that will give you a pokemon at random if the API cannot find the one you typed out. It also gives a conversion telling you how many of a given pokemon you are tall, when it works.
+
 --To-Do--
 
 -basic inputs
--Enter pokemon name
--enter weight
--enter height
+DONE-Enter pokemon name
+DONE-enter weight
+DONE-enter height
 -average both
 -show pokemon stats
 -custom font
 -three.js integration
 -prevent errors for unexpected input
-verify fetches are sanitized with input
+-verify fetches are sanitized with input
+-implement constraints for height/weight/pokemon name length
+-unit options (default metric?)
+person silhouette to compare sizes, keep pokemon same size never change man. Never cut pokemon art off.
+-error message for pokemon not found
--- main.js
+++ main.js
@@ -1,35 +1,74 @@
-const pokeArt = document.getElementById('poke-art');
 
 
 // Fetch pokemon API data.
 async function fetchPokemon(name) {
-    let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-    let data = await response.json()
 
-    console.log(`Height: ${data.height} Weight: ${data.weight}`);
-
-    //console.log(data.sprites.other['official-artwork']['front_default']);
-    pokeArt.src = data.sprites.other['official-artwork']['front_default'];
-    
-    return data;
+    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`).then((response) => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error('Something went wrong');
+    })
+        .then((data) => {
+            console.log(`Height: ${data.height*10}cm Weight: ${data.weight/10}kg`);
+            const pokeArt = document.getElementById('poke-art');
+            pokeArt.src = data.sprites.other['official-artwork']['front_default'];
+        })
+        .catch((error) => {
+            console.log(error)
+            console.log('pokemon not found, selecting at random');
+            fetchRandomPokemon();
+        });
 }
 
+// Fetch a random pokemon
+async function fetchRandomPokemon() {
+
+    // Placeholder random pokemon number selection
+    const selection = Math.floor(Math.random() * 500);
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${selection}/`).then((response) => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error('Something went wrong');
+    })
+        .then((data) => {
+            console.log(`Height: ${data.height*10}cm Weight: ${data.weight/10}kg`);
+            const pokeArt = document.getElementById('poke-art');
+            pokeArt.src = data.sprites.other['official-artwork']['front_default'];
+        })
+        .catch((error) => {
+            console.log(error)
+        });
+}
+
+// Setup default pokemon info to display.
 fetchPokemon('squirtle');
 
-// Add event listener for input particular pokemon names.
-const pokeNameInput = document.getElementById('poke-name-input');
-// pokeNameInput.addEventListener('input', () => {
-//     const input = pokeNameInput.value;
-//     fetchPokemon(`${input}`);
-// });
-
 // Consider entered weight and chosen pokemon and convert to pokemetric.
-const convert = () => {
-    const height = document.getElementById('height-input').value;
+async function convert() {
+    const pokeNameInput = document.getElementById('poke-name-input');
+    const userHeight = document.getElementById('height-input').value;
     const input = pokeNameInput.value;
-    fetchPokemon(`${input}`);
-    console.log(data);
+
+    // Fetch selected pokemon data, then convert user's info to pokemetric.
+    fetchPokemon(`${input}`).then((data) => {
+
+        console.log(data);
+        const factor = (userHeight / (data.height * 10)).toFixed(1);
+        console.log(factor);
+
+        displayResult(`You are as tall as ${factor} ${input}s.`);
+    });
 }
 
+// Display unit conversion info.
+const displayResult = (message) => {
+    const resultBox = document.getElementById('result-info');
+    resultBox.innerText = message;
+}
+
+// Add event listener to make convert button convert units to pokemetric.
 const convertBtn = document.getElementById('convert-btn');
 convertBtn.addEventListener('click', convert);
